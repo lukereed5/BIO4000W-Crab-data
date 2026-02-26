058a12b395a11099ea9da5ea51bddf823b66c63a
lukereed5 <lukeraddonreed@gmail.com>
2026-02-26 20:48:52 +0200

    code

--- Greencrab code.R
+++ Greencrab code.R
@@ -2,7 +2,7 @@ rm(list = ls())
 library(readr)
 install.packages("janitor")
 library(janitor)
-greencrab <-  read_csv("C:/Users/luker/Documents/GIT/BIO4000W-Crab-data/Green Crab Ganzekraal.csv")|>clean_names()
+greencrab <-  read_csv("Green Crab Ganzekraal.csv")|>clean_names()
 
 
 # Convert empty strings to NA
